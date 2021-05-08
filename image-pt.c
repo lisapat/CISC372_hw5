1d64c321ea1b874f16eddba537518edb044b070d
Lisa Pathania <lisapat@br013.ib.bridges2.psc.edu>
2021-05-08 10:46:52 -0400

    added output.txt

--- image-pt.c
+++ image-pt.c
@@ -112,7 +112,7 @@ enum KernelTypes GetKernelType(char* type){
 //main:
 //argv is expected to take 2 arguments.  First is the source file name (can be jpg, png, bmp, tga).  Second is the lower case name of the algorithm.
 int main(int argc,char** argv){
-    long t1,t2;
+    double t1,t2;
     pthread_t *threads;
     
     stbi_set_flip_vertically_on_load(0); 
@@ -164,7 +164,7 @@ int main(int argc,char** argv){
    
    t2 = time(NULL);
 
-    printf("Took %ld seconds\n", t2 - t1);
+    printf("Took %lf seconds\n", t2 - t1);
 
     stbi_write_png("output.png",destImage->width,destImage->height,
 		    destImage->bpp,destImage->data,destImage->bpp*destImage->width);
--- makefile
+++ makefile
@@ -1,5 +1,7 @@
 
-image: image-pt.c image-pt.h
-	gcc -g image-pt.c -o image -lm -pthread
+image-pt: image-pt.c image-pt.h
+	gcc -g image-pt.c -o image-pt -lm -lpthread
+image-mp: image-MP.c
+	gcc -g image-MP.c -o image-mp -lm -fopenmp
 clean:
-	rm -f image output.png
+	rm -f image-pt image-mp output.png
